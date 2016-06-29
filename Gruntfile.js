acc6ebb442fee563ba8b25ecc81b4884e8bbcbcb
Maël Kermann <mael.kermann@weblib.eu>
2016-06-29 15:31:27 +0200

    Reorganize files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,36 @@
 
 module.exports = function (grunt) {
 
-  grunt.initConfig({
-    cssmin: {
-        css : {
-            src: 'dist/yoleth-themepicker.css',
-            dest: 'dist/yoleth-themepicker.min.css'
-        }
-    },
-    uglify: {
-        js : {
-            src: 'dist/yoleth-themepicker.js',
-            dest: 'dist/yoleth-themepicker.min.js'
-        }
-    }
-  });
+    grunt.initConfig({
+        cssmin: {
+            css : {
+                src: 'src/yoleth-themepicker.css',
+                dest: 'dist/yoleth-themepicker.css'
+            }
+        },
+        uglify: {
+            js : {
+                src: 'src/yoleth-themepicker.js',
+                dest: 'dist/yoleth-themepicker.js'
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    flatten: true,
+                    expand: true,
+                    cwd : 'src/',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            }
+        },
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('build', ['cssmin', 'uglify']);
+    grunt.registerTask('build', ['cssmin', 'uglify', 'copy']);
 
 };
